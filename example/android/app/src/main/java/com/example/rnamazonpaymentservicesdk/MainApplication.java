9bbb8d5efdd1ac118aed685f8bce851625095920
Murshid Omer <murshidomer@outlook.com>
2024-08-08 18:19:05 +0300

    Upgrade Gradle and fix compatibilty issues

--- example/android/app/src/main/java/com/example/rnamazonpaymentservicesdk/MainApplication.java
+++ example/android/app/src/main/java/com/example/rnamazonpaymentservicesdk/MainApplication.java
@@ -2,11 +2,7 @@ package com.example.rnamazonpaymentservicesdk;
 
 import android.app.Application;
 import android.content.Context;
-import com.facebook.react.PackageList;
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
-import com.facebook.react.ReactPackage;
-import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.*;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
